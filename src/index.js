3aeb1b8342408d82e68682c0caf10c7bdc7a82cc
Guillaume Pitot <gpitot@nine.com.au>
2019-11-26 11:24:50 +1100

    automatic debug

--- src/index.js
+++ src/index.js
@@ -2,23 +2,17 @@
 find header and footer elements and insert correct header and footer into them
 */
 
-
 import getSite from "./js/site/get-site";
 import setupHeader from "./js/header";
 import setupFooter from "./js/footer";
 
-import './sass/index.scss';
+import "./sass/index.scss";
 
-
-export default ({
-    headerConfig,
-    footerConfig,
-    debug = false
-}) => {
-
-    const site = getSite(debug);
-    if (site === null) return;
-    setupHeader(headerConfig, site);
-    setupFooter(footerConfig, site);
-}
-
+export default ({ headerConfig, footerConfig }) => {
+  //if localhost then debug
+  const debug = window.location.hostname === "localhost";
+  const site = getSite(debug);
+  if (site === null) return;
+  setupHeader(headerConfig, site);
+  setupFooter(footerConfig, site);
+};
